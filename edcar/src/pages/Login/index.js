88c34dd2ae7c2b3ac57b971600ff7d79a382902e
Izabela-dsn <38962331+Izabela-dsn@users.noreply.github.com>
2021-04-24 21:49:56 -0300

    Login adjusted and finished

--- edcar/src/pages/Login/index.js
+++ edcar/src/pages/Login/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Icon } from '@iconify/react';
 import facebookIcon from '@iconify/icons-logos/facebook';
 import google from '@iconify/icons-logos/google-icon';
+import arrowBack from '@iconify/icons-fluent/arrow-step-back-16-regular';
 import './styles.css';
 import banner from '../../assets/images/Login.svg';
 
@@ -11,21 +12,24 @@ function Login (){
         
         <div className="login-page">
             <div className="banner-lateral">
+                <a href="/">
+                    <Icon className="goBack" icon={arrowBack} style={{color: '#fff', fontSize: '32px'}} />
+                </a>
                 <img src={banner} alt="banner lateral"/>
             </div>
 
             <div className="form-login">
-                
+
                 <h1>Login</h1>
 
                 <form>
                     <div className="input-field">
-                        <input type="text" name="email" placeholder="Email"/>
+                        <input type="email" name="email" placeholder="Email"/>
                         <div className="underline"></div>
                     </div>
 
                     <div className="input-field">
-                        <input type="text" name="senha" placeholder="Senha"/>
+                        <input type="password" name="senha" placeholder="Senha"/>
                         <div className="underline"></div>
                     </div>
 
