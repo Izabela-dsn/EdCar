08e12b85e5b96b0595525e96e874a0266516dc73
Izabela-dsn <38962331+Izabela-dsn@users.noreply.github.com>
2021-05-05 18:15:33 -0300

    Card component part 1

---
+++ edcar/src/components/Cards/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles.css';
+
+function Card ({src, alt, type, name, fuel, potency, more, price, onClick}){
+    return(
+        <div className="general-cards" onClick={onClick}>
+            <div className='car-images'>
+                <img src={src} alt={alt}/>
+            </div>
+            <div className='car-information'>
+                <p className="type-car">{type}</p>
+                <p className="car">{name}</p>
+                <p className="car">{fuel}</p>
+                <p className="car">{potency}</p>
+                <p className="car">{more}</p>
+                <p className="car-price">{price}</p>
+            </div>
+        </div>
+    );
+}
+
+Card.propTypes = {
+    src: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    fuel: PropTypes.string.isRequired,
+    potency: PropTypes.string,
+    more: PropTypes.node,
+    price: PropTypes.string,
+    onClick: PropTypes.func,
+};
+
+export default Card;
