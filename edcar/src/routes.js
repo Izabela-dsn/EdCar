e4a5324a7519812881db4744ebb94522201df3de
Izabela-dsn <38962331+Izabela-dsn@users.noreply.github.com>
2021-05-05 18:14:43 -0300

    New routes

--- edcar/src/routes.js
+++ edcar/src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Address from './pages/Valuation/Address';
+import Cars from './pages/Valuation/Car';
 
 function Routes(){
     return(
@@ -10,6 +12,8 @@ function Routes(){
             <Route path="/" exact component={Landing} />
             <Route path="/login" component={Login} />
             <Route path="/cadastro" component={Register}/>
+            <Route path="/reserva/carro" component={Cars}/>
+            <Route path="/reserva/endereco" component={Address}/>
         </BrowserRouter>
     );
 }
