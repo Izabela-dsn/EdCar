3ff8c349f6423e292cd53bc3bf00d86c85add195
Izabela-dsn <38962331+Izabela-dsn@users.noreply.github.com>
2021-06-11 14:20:25 -0300

    login and register

--- edback/src/server.js
+++ edback/src/server.js
@@ -1,6 +1,8 @@
 //json-server
 
+
 import jsonServer from 'json-server';
+
 
 //cria um server - express
 const server = jsonServer.create();
@@ -11,6 +13,7 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
 server.use(middlewares);
+
 server.use(router);
 server.listen(port, () => {
     console.log(`JSON server is running in ${port}`);
